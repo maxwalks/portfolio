032b6ed5e32729cca3b43cfcb0cc8dc161fc7b2d
maxwalks <maximxavierwalker@outlook.com>
2024-02-24 10:54:15 +0100

    Added hire me button

--- init.py
+++ init.py
@@ -1,7 +1,7 @@
 import flask
 from flask import Flask, render_template
 
-#activate_this_file = "/var/www/html/site/venv/bin/activate_this.py"
+#activate_this_file = "/var/www/html/portfolio/venv/bin/activate_this.py"
 #with open(activate_this_file) as _file:
 #        exec(_file.read(), dict(__file__=activate_this_file))
 
