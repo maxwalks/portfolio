bc8bff6997116df1e0fc705bb5de62f8154e3fb6
maxwalks <maximxavierwalker@outlook.com>
2024-02-25 14:17:07 +0100

    Added navigation menu

--- init.py
+++ init.py
@@ -15,5 +15,9 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
 if __name__ == '__main__':
        app.run(debug=True)
